058b40c4c730c3ac86f0c378f670c24c83725f2d
Georgii <eg_egor@mail.ru>
2024-03-06 23:34:30 +0300

    Added config file.

---
+++ config.js
@@ -0,0 +1,5 @@
+export const API_ADDRESS_ARRAY = ['http://api.valantis.store:40000/', 'https://api.valantis.store:41000/'];
+export const PASSWORD = 'Valantis';
+export const ITEMS_PER_PAGE = 50;
+export const FILED_TO_FILTER = ['product', 'price', 'brand'];
+export const RETRY_COUNT = 2;
